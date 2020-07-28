483dfe5d400e2e0681bb750d513a3fa81342bea8
rvdforst <rolf.v.d.forst@fau.de>
2020-07-28 17:46:22 +0200

    Add vars + actions functions

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,6 +1,58 @@
 "use strict";
 
 jQuery(document).ready(function () {
+    var DATE_FORMAT = rrze_rsvp_admin.dateformat,
+        TEXT_DELETE = rrze_rsvp_admin.text_delete,
+        TEXT_CANCELLED = rrze_rsvp_admin.text_cancelled,
+        TEXT_CONFIRMED = rrze_rsvp_admin.text_confirmed,
+        API_AJAXURL = rrze_rsvp_admin.ajaxurl;
+
+	function executeAction(type, button) {
+		var id = button.attr('data-id'),
+			href = button.attr('href');
+
+		jQuery.ajax({
+			type: "POST",
+			url: API_AJAXURL,
+			data: {
+				action: 'execute_action',
+				id: id,
+				type: type
+			}
+		}).fail(function (jqXHR) {
+			console.error("AJAX request failed");
+		}).done(function(data) {
+			data = JSON.parse(data);
+			jQuery.ajax({
+				type: "GET",
+				url: href,
+			}).fail(function (jqXHR) {
+				console.error("AJAX request failed");
+			}).done(function(data) {
+				if (type == 'confirm') {
+					button.addClass('rrze-rsvp-confirmed').attr('disabled', 'disabled').html(TEXT_CONFIRMED);
+				} else {
+					button.attr('disabled', 'disabled').html(TEXT_CANCELLED);
+				}
+			});
+
+		});
+
+	}
+
+    jQuery('.rrze-rsvp-bookings .rrze-rsvp-confirm').click(function(e) {
+        e.preventDefault();
+        executeAction('confirm', jQuery(this));
+        return false;
+    });
+
+    jQuery('.rrze-rsvp-bookings .rrze-rsvp-cancel').click(function(e) {
+        e.preventDefault();
+        if (confirm(TEXT_DELETE)) {
+            executeAction('cancel', jQuery(this));
+        }
+        return false;
+    });
 
     jQuery('.rrze_rsvp_datepicker').each(function (i, v) {
         var el = jQuery(v);
