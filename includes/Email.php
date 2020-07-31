fa4c4fa6b5384fba1fbdaee9dd446990b299cfcc
rvdforst <rolf.v.d.forst@fau.de>
2020-07-31 23:09:10 +0200

    Refactoring

--- includes/Email.php
+++ includes/Email.php
@@ -6,23 +6,43 @@ defined('ABSPATH') || exit;
 
 class Email
 {
+    /**
+     * Options
+     * @var object
+     */
     protected $options;
 
+    /**
+     * Template object
+     * @var object
+     */
     protected $template;
 
-    public function __construct() {
+    /**
+     * __construct
+     */
+    public function __construct()
+    {
         $settings = new Settings(plugin()->getFile());
         $this->options = (object) $settings->getOptions();
         $this->template = new Template;
     }
 
+    /**
+     * send
+     * Send an email.
+     * @param string $to
+     * @param string $subject
+     * @param string $message
+     * @return void
+     */
     public function send(string $to, string $subject, string $message)
     {
         $data = [
             'subject' => $subject,
             'message' => $message
         ];
-        $body = $this->template->getContent('email-body', $data);
+        $body = $this->template->getContent('email/email-body', $data);
 
         $headers = [
             'Content-type: text/html; charset=utf-8'
@@ -37,40 +57,74 @@ class Email
         remove_filter('wp_mail_from', [$this, 'filterEmail']);
     }
 
-    public function filterEmail($from)
+    /**
+     * filterEmail
+     * Callable function of the hook 'wp_mail_from'. 
+     * Filters the email address to send from.
+     * @param string $from Sender's email address
+     * @return string
+     */
+    public function filterEmail($from): string
     {
         $newFrom = $this->options->email_sender_email;
         return ($newFrom != '') ? $newFrom : $from;
     }
 
-    public function filterName($name)
+    /**
+     * filterName
+     * Callable function of the hook 'wp_mail_from_name'.
+     * Filters the name to associate with the "from" email address.
+     * @param string $name Sender's name
+     * @return string
+     */
+    public function filterName($name): string
     {
         $newName = $this->options->email_sender_name;
         return ($newName != '') ? $newName : $name;
     }
 
-    public function bookingNew(string $to, string $subject, int $bookingId)
+    /**
+     * bookingRequestedAdmin
+     * Send an email to the admin if a new booking is requested. Optionally, 
+     * the admin can confirm or cancel the reservation using the corresponding 
+     * links included in the email message.
+     * @param string $to Customer email address
+     * @param string $subject Email subject
+     * @param integer $bookingId Booking Id
+     * @return void
+     */
+    public function bookingRequestedAdmin(string $to, string $subject, int $bookingId)
     {
         $booking = Functions::getBooking($bookingId);
         if (empty($booking)) {
             return;
         }
 
+        $confirmUrl = Functions::bookingReplyUrl('confirm', $booking['booking_date'], $bookingId);
+        $cancelUrl = Functions::bookingReplyUrl('cancel', $booking['booking_date'], $bookingId);
+
         $booking['text'] = sprintf(__('You received a new request for a booking on %s.', 'rrze-rsvp'), get_bloginfo('name'));
 
         $booking['user_info'] = '';
 
-        $booking['confirm_url'] = '';
+        $booking['confirm_url'] = $confirmUrl;
         $booking['confirm_link_text'] = __('Confirm Booking', 'rrze-rsvp');
-        $booking['cancel_url'] = '';
+
+        $booking['cancel_url'] = $cancelUrl;
         $booking['cancel_link_text'] = __('Cancel Booking', 'rrze-rsvp');
 
-        $message = $this->template->getContent('email-new', $booking);
+        $message = $this->template->getContent('email/booking-requested-admin', $booking);
 
         $this->send($to, $subject, $message);
     }
 
-    public function bookingCancelNotification(int $bookingId)
+    /**
+     * bookingCancelledAdmin
+     * Send an email to the admin when the customer cancels the booking.
+     * @param integer $bookingId Booking Id
+     * @return void
+     */
+    public function bookingCancelledAdmin(int $bookingId)
     {
         $booking = Functions::getBooking($bookingId);
         if (empty($booking)) {
@@ -84,20 +138,23 @@ class Email
         $subject = __('Booking has been cancelled', 'rrze-rsvp');
         $text = sprintf(__('A booking on %s has been cancelled by the customer.', 'rrze-rsvp'), get_bloginfo('name'));
 
-        $optionsData = [
-            'subject' => $subject,
-            'text' => $text
-        ];
+        $booking['text'] = $this->placeholderParser($text, $booking);
 
-        $subject = $this->placeholderParser($subject, $optionsData);
-        $booking['text'] = $this->placeholderParser($text, $optionsData);
-
-        $message = $this->template->getContent('email-cancel-notification', $booking);
+        $message = $this->template->getContent('email/booking-cancelled-admin', $booking);
 
         $this->send($to, $subject, $message);
     }
 
-    public function bookingReceived(int $bookingId)
+    /**
+     * bookingRequestedCustomer
+     * Send an email to the customer if a new booking is requested. The booking 
+     * is not yet confirmed. An admin action is required for confirmation or 
+     * cancellation of the booking. Optionally, the customer can cancel the 
+     * reservation using the link included in the email message.
+     * @param integer $bookingId
+     * @return void
+     */
+    public function bookingRequestedCustomer(int $bookingId)
     {
         $booking = Functions::getBooking($bookingId);
         if (empty($booking)) {
@@ -107,23 +164,25 @@ class Email
         $subject = $this->options->email_received_subject;
         $text = $this->options->email_received_text;
 
-        $optionsData = [
-            'subject' => $subject,
-            'text' => $text
-        ];
+        $subject = $this->placeholderParser($subject, $booking);
+        $booking['text'] = $this->placeholderParser($text, $booking);
 
-        $subject = $this->placeholderParser($subject, $optionsData);
-        $booking['text'] = $this->placeholderParser($text, $optionsData);
-
-        $cancelUrl = '';
+        $cancelUrl = Functions::bookingReplyUrl('cancel', $booking['booking_date'] . '-customer', $bookingId);
         $booking['cancel_booking'] = sprintf(__('Please <a href="%s">cancel your booking</a> in time if your plans change.', 'rzze-rsvp'), $cancelUrl) . '</p>';
 
-        $message = $this->template->getContent('email-received', $booking);
+        $message = $this->template->getContent('email/booking-requested-customer', $booking);
 
         $this->send($booking['guest_email'], $subject, $message);
     }
 
-    public function bookingConfirmed(int $bookingId)
+    /**
+     * bookingConfirmedCustomer
+     * Send a booking confirmation email to the customer. Optionally, the
+     * customer can cancel the booking using the link included in the email message.
+     * @param integer $bookingId Booking Id
+     * @return void
+     */
+    public function bookingConfirmedCustomer(int $bookingId)
     {
         $booking = Functions::getBooking($bookingId);
         if (empty($booking)) {
@@ -133,23 +192,25 @@ class Email
         $subject = $this->options->email_confirm_subject;
         $text = $this->options->email_confirm_text;
 
-        $optionsData = [
-            'subject' => $subject,
-            'text' => $text
-        ];
+        $subject = $this->placeholderParser($subject, $booking);
+        $booking['text'] = $this->placeholderParser($text, $booking);
 
-        $subject = $this->placeholderParser($subject, $optionsData);
-        $booking['text'] = $this->placeholderParser($text, $optionsData);
-
-        $cancelUrl = '';
+        $cancelUrl = Functions::bookingReplyUrl('cancel', $booking['booking_date'] . '-customer', $bookingId);
         $booking['cancel_booking'] = sprintf(__('Please <a href="%s">cancel your booking</a> in time if your plans change.', 'rzze-rsvp'), $cancelUrl) . '</p>';
 
-        $message = $this->template->getContent('email-cancel', $booking);
+        $message = $this->template->getContent('email/booking-confirmed-customer', $booking);
 
         $this->send($booking['guest_email'], $subject, $message);
     }
 
-    public function bookingCancelled(int $bookingId)
+    /**
+     * bookingCancelledCustomer
+     * Send a booking cancellation email to the client. No further action 
+     * is necessary.
+     * @param integer $bookingId Booking Id
+     * @return void
+     */
+    public function bookingCancelledCustomer(int $bookingId)
     {
         $booking = Functions::getBooking($bookingId);
         if (empty($booking)) {
@@ -159,26 +220,37 @@ class Email
         $subject = $this->options->email_cancel_subject;
         $text = $this->options->email_cancel_text;
 
-        $optionsData = [
-            'subject' => $subject,
-            'text' => $text
-        ];
+        $subject = $this->placeholderParser($subject, $booking);
+        $booking['text'] = $this->placeholderParser($text, $booking);
 
-        $subject = $this->placeholderParser($subject, $optionsData);
-        $booking['text'] = $this->placeholderParser($text, $optionsData);
-
-        $message = $this->template->getContent('email-cancel', $booking);
+        $message = $this->template->getContent('email/booking-cancelled-customer', $booking);
 
         $this->send($booking['guest_email'], $subject, $message);
     }
 
-    protected function placeholderParser(string $text, array $data): string
+    /**
+     * placeholderParser
+     * YA Email Template Parser.
+     * @param string $text
+     * @param array $booking Booking data
+     * @return string
+     */
+    protected function placeholderParser(string $text, array $booking): string
     {
+        $data = [
+            'date' => $booking['date'],
+            'time' => $booking['time'],
+            'room_name' => $booking['room_name'],
+            'seat_name' => $booking['seat_name'],
+            'guest_name' => $booking['guest_firstname'] . ' ' . $booking['guest_lastname'],
+            'guest_email' => $booking['guest_email'],
+            'guest_phone' => $booking['guest_phone']
+        ];
+
         foreach ($data as $key => $field) {
             $text = str_replace('{{' . $key . '}}', $field, $text);
         }
         $text = preg_replace('%\{\{.+\}\}%', '', $text);
         return $text;
     }
-
 }
