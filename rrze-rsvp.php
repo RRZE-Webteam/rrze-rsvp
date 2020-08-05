170602fd5bfb98b60fc95cf462f9353d529f481e
Wolfgang Wiese <xwolf@xwolf.de>
2020-08-05 10:05:24 +0200

    Sprachupdates

--- rrze-rsvp.php
+++ rrze-rsvp.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE-RSVP
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-rsvp
 Description:     Booking system.
-Version:         0.0.13
+Version:         0.0.14-0
 Author:          RRZE-Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
