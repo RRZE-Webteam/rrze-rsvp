adbc027a183c7ea9819fc48196e9bb64e418345a
rvdforst <rolf.v.d.forst@fau.de>
2020-08-10 09:08:06 +0200

    Add every 5 min event; add deleteCancelledBookings();  add cancelNotCheckedInBookings()

--- includes/Schedule.php
+++ includes/Schedule.php
@@ -6,44 +6,100 @@ defined('ABSPATH') || exit;
 
 use WP_Query;
 
+/**
+ * Schedule
+ * Manage the cron schedules
+ */
 class Schedule
 {
+    /**
+     * Email object
+     * @var object
+     */
     protected $email;
 
+    /**
+     * __construct
+     */
     public function __construct()
     {
         $this->email = new Email;
     }
 
+    /**
+     * onLoaded
+     * Launch all hooks.
+     * @return void
+     */
     public function onLoaded()
     {
-        add_action('wp', [$this, 'activateSchedule']);
-        add_action('rrze_rsvp_hourly_event', [$this, 'hourlyEvent']);
+        add_action('wp', [$this, 'activateScheduledEvents']);
+        add_filter('cron_schedules', [$this, 'customCronSchedules']);
+        add_action('rrze_rsvp_every5minutes_event', [$this, 'Every5MinutesEvent']);
         add_action('rrze_rsvp_daily_event', [$this, 'dailyEvent']);
     }
 
-    public function activateSchedule()
+    /**
+     * customCronSchedules
+     * Add custom cron schedules.
+     * @param array $schedules Available cron schedules
+     * @return array New cron schedules
+     */
+    public function customCronSchedules(array $schedules): array
     {
-        if (!wp_next_scheduled('rrze_rsvp_hourly_event')) {
-            wp_schedule_event(time(), 'hourly', 'rrze_rsvp_hourly_event');
+        $schedules['rrze_rsvp_every5minutes'] = [
+            'interval' => 5 * MINUTE_IN_SECONDS,
+            'display' => __('Every five minutes', 'rrze-rsvp')
+        ];
+        return $schedules;
+    }
+
+    /**
+     * activateScheduledEvents
+     * Activate all scheduled events.
+     * @return void
+     */
+    public function activateScheduledEvents()
+    {
+        if (!wp_next_scheduled('rrze_rsvp_every5minutes_event')) {
+            wp_schedule_event(time(), 'rrze_rsvp_every5minutes', 'rrze_rsvp_every5minutes_event');
         }
         if (!wp_next_scheduled('rrze_rsvp_daily_event')) {
             wp_schedule_event(time(), 'daily', 'rrze_rsvp_daily_event');
         }
     }
 
-    public function hourlyEvent()
+    /**
+     * Every5MinutesEvent
+     * Run the event every 5 minutes.
+     * @return void
+     */
+    public function Every5MinutesEvent()
     {
         $this->cancelUnconfirmedBookings();
+        $this->cancelNotCheckedInBookings();
     }
 
+    /**
+     * dailyEvent
+     * Run the event daily.
+     * @return void
+     */    
     public function dailyEvent()
     {
         $this->deleteOldBookings();
+        $this->deleteCancelledBookings();
     }
 
+    /**
+     * deleteOldBookings
+     * Delete all reservations whose start datetime is older than 4 months.
+     * @return void
+     */
     protected function deleteOldBookings()
     {
+        $timeStampBefore = current_time('timestamp') - (4 * WEEK_IN_SECONDS);
+
         $args = [
             'fields'            => 'ids',
             'post_type'         => ['booking'],
@@ -52,7 +108,7 @@ class Schedule
             'meta_query'        => [
                 [
                     'key'       => 'rrze-rsvp-booking-start',
-                    'value'     => current_time('timestamp') - (4 * WEEK_IN_SECONDS),
+                    'value'     => $timeStampBefore,
                     'compare'   => '<'
                 ]
             ]
@@ -69,29 +125,79 @@ class Schedule
         }
     }
 
-    protected function cancelUnconfirmedBookings()
+    /**
+     * deleteCancelledBookings
+     * Delete bookings that were cancelled more than 1 day ago.
+     * @return void
+     */
+    protected function deleteCancelledBookings()
     {
+        $timeStampBefore = current_time('timestamp') - (1 * DAY_IN_SECONDS);
+
         $args = [
             'fields'            => 'ids',
             'post_type'         => ['booking'],
             'post_status'       => 'publish',
             'posts_per_page'    => '-1',
-            'meta_query' => [
-                'relation' => 'AND',
-                'user_status_clause' => [
-                    'key' => 'rrze-rsvp-customer-status',
-                    'value' => 'confirmed',
-                    'compare' => '!='
+            'date_query'        => [
+                [
+                    'before'    => date('Y-m-d H:i:s', $timeStampBefore),
+                    'inclusive' => false,
                 ],
-                'status_clause' => [
-                    'key' => 'rrze-rsvp-booking-status',
-                    'value' => 'booked',
-                    'compare' => '='
+            ],
+            'meta_query'        => [
+                [
+                    'key'       => 'rrze-rsvp-booking-status',
+                    'value'     => ['cancelled'],
+                    'compare'   => 'IN'
+                ]
+            ]
+        ];
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                wp_delete_post(get_the_ID(), true);
+            }
+            wp_reset_postdata();
+        }
+    }
+
+    /**
+     * cancelUnconfirmedBookings
+     * Cancel bookings with a date greater than 1 hour that were not 
+     * confirmed by the customer. It depends on whether the request for 
+     * confirmation by the customer is activated in the room settings.
+     * @return void
+     */
+    protected function cancelUnconfirmedBookings()
+    {
+        $timeStampBefore = current_time('timestamp') - (1 * HOUR_IN_SECONDS);
+
+        $args = [
+            'fields'            => 'ids',
+            'post_type'         => ['booking'],
+            'post_status'       => 'publish',
+            'posts_per_page'    => '-1',
+            'date_query'        => [
+                [
+                    'before'    => date('Y-m-d H:i:s', $timeStampBefore),
+                    'inclusive' => false,
                 ],
-                'status_start' => [
-                    'key'       => 'rrze-rsvp-booking-start',
-                    'value'     => current_time('timestamp') - (1 * HOUR_IN_SECONDS),
-                    'compare'   => '<'
+            ],
+            'meta_query'        => [
+                'relation'      => 'AND',
+                'customer_status_clause' => [
+                    'key'       => 'rrze-rsvp-customer-status',
+                    'value'     => 'booked',
+                    'compare'   => '='
+                ],
+                'booking_status_clause' => [
+                    'key'       => 'rrze-rsvp-booking-status',
+                    'value'     => ['booked', 'confirmed'],
+                    'compare'   => 'IN'
                 ]
             ]
         ];
@@ -103,6 +209,52 @@ class Schedule
                 $query->the_post();
                 $roomId = get_post_meta(get_the_ID(), 'rrze-rsvp-seat-room', true);
                 if (get_post_meta($roomId, 'rrze-rsvp-room-force-to-confirm', true)) {
+                    update_post_meta(get_the_ID(), 'rrze-rsvp-booking-status', 'cancelled');
+                    $this->email->bookingCancelledCustomer(get_the_ID());
+                }
+            }
+            wp_reset_postdata();
+        }
+    }
+
+    /**
+     * cancelNotCheckedInBookings
+     * Cancel booking that were not checked-in after 15 minutes 
+     * of starting the event. It depends on whether the check-in requirement 
+     * is activated in the room settings.
+     * @return void
+     */
+    protected function cancelNotCheckedInBookings()
+    {
+        $timeStampAfter = current_time('timestamp') + (15 * MINUTE_IN_SECONDS);
+
+        $args = [
+            'fields'            => 'ids',
+            'post_type'         => ['booking'],
+            'post_status'       => 'publish',
+            'posts_per_page'    => '-1',
+            'meta_query'        => [
+                'relation'      => 'AND',
+                'booking_status_clause' => [
+                    'key'       => 'rrze-rsvp-booking-status',
+                    'value'     => ['confirmed'],
+                    'compare'   => 'IN'
+                ],
+                'booking_start_clause' => [
+                    'key'       => 'rrze-rsvp-booking-start',
+                    'value'     => $timeStampAfter,
+                    'compare'   => '<'
+                ]                
+            ]
+        ];
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $roomId = get_post_meta(get_the_ID(), 'rrze-rsvp-seat-room', true);
+                if (get_post_meta($roomId, 'rrze-rsvp-room-force-to-checkin', true)) {
                     update_post_meta(get_the_ID(), 'rrze-rsvp-booking-status', 'cancelled');
                     $this->email->bookingCancelledCustomer(get_the_ID());
                 }
