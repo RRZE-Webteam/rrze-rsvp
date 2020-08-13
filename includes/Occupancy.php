edda1a66d17c2d62056b09215696085e4db5e98a
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-08-13 16:13:50 +0200

    submenu 'Room occupancy' in CPT

--- includes/Occupancy.php
+++ includes/Occupancy.php
@@ -5,53 +5,18 @@ namespace RRZE\RSVP;
 defined('ABSPATH') || exit;
 
 use RRZE\RSVP\Functions;
-// use RRZE\RSVP\Capabilities;
 
 
-// if (isset($_POST['rsvp_room_id'])){
-//     echo '<pre>';
-//     echo 'echo rsvp_room_id posted';
-//     $roomId = filter_input(INPUT_POST, 'rsvp_room_id', FILTER_VALIDATE_INT);
-//     $response = Functions::getOccupancyByRoomIdHTML($roomId);
-//     var_dump($response);
-//     exit;
-// }
 
 class Occupancy{
-    /**
-     * Options
-     * @var object
-     */
-    protected $options;
 
-
-    /**
-     * __construct
-     */
     public function __construct(){
-        // $settings = new Settings(plugin()->getFile());
-        // $this->options = (object) $settings->getOptions();
     }
 
     public function onLoaded(){
-        add_action( 'admin_menu', [$this, 'registerOccupancyPage'] );
         add_action( 'admin_enqueue_scripts', [$this, 'adminEnqueueScripts']);
         add_action( 'wp_ajax_ShowOccupancy', [$this, 'ajaxGetOccupancy'] );
         add_action( 'wp_ajax_nopriv_ShowOccupancy', [$this, 'ajaxGetOccupancy'] );
-    }
-
-    public function registerOccupancyPage(){
-        // $cpts = array_keys(Capabilities::getCurrentCptArgs());
-
-        add_submenu_page(
-            'edit.php?post_type=booking',      // parent slug
-            __( 'Current room occupancy', 'rrze-rsvp' ),            // page title
-            __( 'Room occupancy', 'rrze-rsvp' ),       // menu title
-            'manage_options',         // capability
-            'rrze-rsvp', // 'rrze-rsvp'
-            [$this, 'getOccupancyPage'],
-            2      
-        );
     }
 
     public function adminEnqueueScripts(){
@@ -75,38 +40,4 @@ class Occupancy{
         $response = Functions::getOccupancyByRoomIdHTML($roomId);
         wp_send_json($response);
     }
-
-
-    public function getOccupancyPage(){
-        echo '<div class="wrap">';
-        echo '<h1>' . esc_html_x( 'Current room occupancy', 'admin page title', 'rrze-rsvp' ) . '</h1>';
-        echo '<table class="form-table" role="presentation"><tbody>';
-        echo '<tr><th scope="row"><label for="select_room">' . __('Room','rrze-rsvp') . '</label></th>';
-        echo '<td>'
-            . '<form action="" method="post" class="occupancy">'
-            . '<select id="rsvp_room_id" name="rsvp_room_id">'
-            . '<option>&mdash; ' . __('Please select', 'rrze-rsvp') . ' &mdash;</option>';
-
-        $rooms = get_posts([
-            'post_type' => 'room',
-            'post_statue' => 'publish',
-            'nopaging' => true,
-            'orderby' => 'title',
-            'order' => 'ASC',
-        ]);
-
-        foreach ($rooms as $room) {
-            echo '<option value="' . $room->ID . '">' . $room->post_title . '</option>';
-        }
-
-        echo '</select></form></td></tr>';
-        // echo '</select> <input type="submit" value="Submit it"></form></td></tr>';
-        echo '</tbody></table>';
-        echo '<div id="loading"><i class="fa fa-refresh fa-spin fa-4x"></i></div>';
-
-        echo '<div class="rsvp-occupancy-container"></div>';
-        
-        echo '</div>';
-     }
-
 }
