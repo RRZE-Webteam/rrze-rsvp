cb9018a9fdd9f4fb7f78d215484886221089a416
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-08-19 15:14:33 +0200

    bugfix "create seats"

--- src/js/rrze-rsvp-seat.js
+++ src/js/rrze-rsvp-seat.js
@@ -1,3 +1,3 @@
 jQuery(document).ready( function($) {
-    $(".wrap .page-title-action").after("<a class='page-title-action' href='wp-admin/tools.php?page=rrze-rsvp'>" + button_label + "</a>");
+    $(".wrap .page-title-action").after("<a class='page-title-action' href='tools.php?page=rrze-rsvp'>" + button_label + "</a>");
 });
