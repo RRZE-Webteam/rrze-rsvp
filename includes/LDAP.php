699983c83c1b55e4438e9915b917e315e796b29a
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-09-25 17:18:10 +0200

    https://github.com/RRZE-Webteam/rrze-rsvp/issues/24 (in development)

--- includes/LDAP.php
+++ includes/LDAP.php
@@ -8,51 +8,70 @@ use RRZE\RSVP\Settings;
 
 class LDAP {
     protected $settings;
-    protected $ldap_server;
-    protected $ldap_domain;
-
+    protected $connection;
+    protected $server;
+    protected $port;
+    protected $domain;
+    protected $distinguished_name;
+    protected $base_dn;
+    protected $filter;
+    protected $attributes;
 
     public function __construct() {
         $this->settings = new Settings(plugin()->getFile());
-        $this->ldap_server = $this->settings->getOption('ldap', 'server');
-        $this->ldap_domain = $this->settings->getOption('ldap', 'domain');
-    }
+        // $this->server = $this->settings->getOption('ldap', 'server');
+        // $this->base_dn = $this->settings->getOption('ldap', 'domain');
 
+        $this->server = 'ubaddc1.bib.uni-erlangen.de';
+        $this->port = '3268';
+        // $this->distinguished_name = 'CN=UB Bib User,OU=Groups,OU=UB,DC=ubad,DC=fau,DC=de';
+        $this->distinguished_name = 'CN=UB_Bib_User,OU=Groups,OU=UB,DC=ubad,DC=fau,DC=de';
+        // $this->base_dn = 'ubad.fau.de:3269';
+        $this->base_dn = 'ubad.fau.de';
+        // $this->filter = 'UB_Bib_User';
+        $this->filter = 'sAMAccountName';
+        $this->attributes = array("mail", "displayName", "givenName", "title");
+    }
 
     public function onLoaded() {
         add_shortcode('rsvp-ldap-test', [$this, 'ldapTest'], 10, 2);
     }
     
+    private function logError(string $method): string{
+        $msg = 'LDAP-error ' . ldap_errno($this->connection) . ' ' . ldap_error($this->connection) . " using $method | server = {$this->server}:{$this->port}";
+        do_action('rrze.log.error', 'rrze-rsvp : ' . $msg);
+        return $msg;
+    }
 
     public function ldapTest($atts, $content = ''){
         if(isset($_POST['username']) && isset($_POST['password'])){
-            $ldapconn = ldap_connect($this->ldap_server);
+            $this->connection = ldap_connect($this->server, $this->port);
         
-            if (!$ldapconn){
-                $content = 'LDAP-error no.' . ldap_errno($ldapconn) . ': ' . ldap_error($ldapconn);
-                do_action('rrze.log.error', 'rrze-rsvp : ldapTest() : ' . $content . ' using ldap_connect() with ' . $this->ldap_server);
+            if (!$this->connection){
+                $content = $this->logError('ldap_connect()');
             }else{
-                ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-                ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+                ldap_set_option($this->connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+                ldap_set_option($this->connection, LDAP_OPT_REFERRALS, 0);
                 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
                 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
             
-                // $username = $this->ldap_domain . '\\' . $username;
-                // $username = $this->ldap_domain . '@' . $username;
-            
-                $bind = @ldap_bind($ldapconn, $username, $password);
+                // $bind = @ldap_bind($this->connection, $username, $password);
+                $bind = @ldap_bind($this->connection, $this->base_dn . ',' . $username, $password);
+                // $bind = @ldap_bind($this->connection, $this->base_dn . '\\' . $username, $password);
+                // $bind = @ldap_bind($this->connection, $this->base_dn . '@' . $username, $password);
+                // $bind = @ldap_bind($this->connection, $username . ',' . $this->base_dn, $password);
+                // $bind = @ldap_bind($this->connection, $username . '\\' . $this->base_dn, $password);
+                // $bind = @ldap_bind($this->connection, $username . '@' . $this->base_dn, $password);
+                // => none of these 7 variants do work : $username &| $password is wrong
 
                 if (!$bind) {
-                    $content = 'LDAP-error no.' . ldap_errno($ldapconn) . ': ' . ldap_error($ldapconn);
-                    do_action('rrze.log.error', 'rrze-rsvp : ldapTest() : ' . $content . ' using ldap_bind()');
+                    $content = $this->logError('ldap_bind()');
                 }else{
-                    $filter="(sAMAccountName=$username)";
-                    $result = @ldap_search($ldapconn,"dc=MYDOMAIN,dc=COM",$filter);
+                    $result = @ldap_search( $this->connection, $this->distinguished_name, "({$this->filter}=$username)");
                     if ($result === false){
-                        $content = 'LDAP-error no.' . ldap_errno($ldapconn) . ': ' . ldap_error($ldapconn);
-                        do_action('rrze.log.error', 'rrze-rsvp : ldapTest() : ' . $content . ' using ldap_search()');
+                        $content = $this->logError('ldap_search()');
                     }else{
-                        $info = @ldap_get_entries($ldapconn, $result);
+                        $info = @ldap_get_entries($this->connection, $result);
                         // return_value["count"] = number of entries in the result
                         // return_value[0] : refers to the details of first entry
                         // return_value[i]["dn"] =  DN of the ith entry in the result
@@ -70,7 +89,7 @@ class LDAP {
                         if (!$content){
                             $content = 'User not found';
                         }
-                        @ldap_close($ldapconn);
+                        @ldap_close($this->connection);
                     }
                 }
             }
