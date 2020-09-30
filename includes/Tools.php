34987116a09208c94034eca617fa1b8b8764bfc7
Cassandre <info@barbara-bothe.de>
2020-09-30 15:59:35 +0200

    Bugfix Platzgenerator

--- includes/Tools.php
+++ includes/Tools.php
@@ -40,84 +40,86 @@ class Tools
         echo '<div class="wrap">';
         echo '<h1>' . esc_html_x( 'Create Seats', 'admin page title', 'rrze-rsvp' ) . '</h1>';
 
-        /*
-         * Submit Form, Create Seats
-         */
-        
-        $errors = [];
-        if ($_GET[ 'room_id' ] == '') {
-            $errors[] = __('Please select a room.');
-        }
-        if (($_GET[ 'number_rows' ] == '')) {
-            $errors[] = __('Please specify the number of rows.');
-        }
-        if ( ! isset($_GET[ 'schema_rows' ])) {
-            $errors[] = __('Please specify the rows numbering system.');
-        }
-        if (($_GET[ 'number_seats' ] == '')) {
-            $errors[] = __('Please specify the number of seats.');
-        }
-        if ( ! isset($_GET[ 'schema_seats' ])) {
-            $errors[] = __('Please specify the seats numbering system.');
-        }
-        if (!empty($errors)) {
-            echo '<div class="notice notice-error">';
-            echo '<h2>' . __('An error occurred') . '</h2>';
-            echo '<p>' . explode('<br />', $errors) . '</p>';
-            echo '</div>';
-        } else {
-            $room_id = isset($_GET['room_id']) ? absint($_GET['room_id']) : '';
-            $num_rows = isset($_GET['number_rows']) ? absint($_GET['number_rows']) : 0;
-            $num_seats = isset($_GET['number_rows']) ? absint($_GET['number_seats']) : 0;
-            $row = (isset($_GET['schema_rows']) && $_GET['schema_rows'] == 'a-z') ? 'A' : 1;
-            $seat = (isset($_GET['schema_seats']) && $_GET['schema_seats'] == 'num') ? 1 : 'A';
-            $seat_start = $seat;
-            if ($row == $seat) {
-                $label_row = _x('Row', 'part of the generated seat name', 'rrze-rsvp') . ' ';
-                $label_seat = ' - ' . _x('Seat', 'part of the generated seat name', 'rrze-rsvp') . ' ';
-            } else {
-                $label_row = '';
-                $label_seat = '';
-            }
-            if ($_GET['schema_rows'] == 'num' && isset($_GET['start_number']) && is_numeric($_GET['start_number'])) {
-                $row = absint($_GET['start_number']);
-            }
-            if (isset($_GET['equipment'])) {
-                $equipment = array_map('absint', $_GET['equipment']);
-            } else {
-                $equipment = false;
-            }
-            $prefix = isset($_GET['prefix']) ? sanitize_text_field($_GET['prefix']) . ' ' : '';
-            $num_created = 0;
+        if ( isset( $_GET['status']) && $_GET['status'] == 'submit') {
+            /*
+             * Submit Form, Create Seats
+             */
 
-            for ($i = 1; $i <= $num_rows; $i++) {
-                for ($j = 1; $j <= $num_seats; $j++) {
-                    $seat_name = $prefix . $label_row . $row . $label_seat . $seat;
-                    $new_seat = [
-                        'post_status' => 'publish',
-                        'post_type' => 'seat',
-                        'post_title' => $seat_name,
-                    ];
-                    $seat_id = wp_insert_post($new_seat);
-                    $num_created++;
-                    if ($seat_id) {
-                        update_post_meta($seat_id, 'rrze-rsvp-seat-room', $room_id);
-                        if (is_array($equipment)) {
-                            wp_set_post_terms($seat_id, $equipment, 'rrze-rsvp-equipment');
-                        }
-                    }
-                    $seat++;
+            $errors = [];
+            if ($_GET['room_id'] == '') {
+                $errors[] = __('Please select a room.');
+            }
+            if (($_GET['number_rows'] == '')) {
+                $errors[] = __('Please specify the number of rows.');
+            }
+            if (!isset($_GET['schema_rows'])) {
+                $errors[] = __('Please specify the rows numbering system.');
+            }
+            if (($_GET['number_seats'] == '')) {
+                $errors[] = __('Please specify the number of seats.');
+            }
+            if (!isset($_GET['schema_seats'])) {
+                $errors[] = __('Please specify the seats numbering system.');
+            }
+            if (!empty($errors)) {
+                echo '<div class="notice notice-error">';
+                echo '<h2>' . __('An error occurred') . '</h2>';
+                echo '<p>' . explode('<br />', $errors) . '</p>';
+                echo '</div>';
+            } else {
+                $room_id = isset($_GET['room_id']) ? absint($_GET['room_id']) : '';
+                $num_rows = isset($_GET['number_rows']) ? absint($_GET['number_rows']) : 0;
+                $num_seats = isset($_GET['number_rows']) ? absint($_GET['number_seats']) : 0;
+                $row = (isset($_GET['schema_rows']) && $_GET['schema_rows'] == 'a-z') ? 'A' : 1;
+                $seat = (isset($_GET['schema_seats']) && $_GET['schema_seats'] == 'num') ? 1 : 'A';
+                $seat_start = $seat;
+                if ($row == $seat) {
+                    $label_row = _x('Row', 'part of the generated seat name', 'rrze-rsvp') . ' ';
+                    $label_seat = ' - ' . _x('Seat', 'part of the generated seat name', 'rrze-rsvp') . ' ';
+                } else {
+                    $label_row = '';
+                    $label_seat = '';
                 }
-                $row++;
-                $seat = $seat_start;
-            }
+                if ($_GET['schema_rows'] == 'num' && isset($_GET['start_number']) && is_numeric($_GET['start_number'])) {
+                    $row = absint($_GET['start_number']);
+                }
+                if (isset($_GET['equipment'])) {
+                    $equipment = array_map('absint', $_GET['equipment']);
+                } else {
+                    $equipment = false;
+                }
+                $prefix = isset($_GET['prefix']) ? sanitize_text_field($_GET['prefix']) . ' ' : '';
+                $num_created = 0;
 
-            echo '<div class="notice notice-success is-dismissible">';
-            echo '<h2>' . __('Done!', 'rrze-rsvp') . '</h2>';
-            if ($num_created > 0) {
-                echo '<p>' . sprintf(_n( '%s new seat has been created.', '%s new seats have been created.', $num_created, 'rrze-rsvp'), $num_created) . '</p>';
+                for ($i = 1; $i <= $num_rows; $i++) {
+                    for ($j = 1; $j <= $num_seats; $j++) {
+                        $seat_name = $prefix . $label_row . $row . $label_seat . $seat;
+                        $new_seat = [
+                            'post_status' => 'publish',
+                            'post_type' => 'seat',
+                            'post_title' => $seat_name,
+                        ];
+                        $seat_id = wp_insert_post($new_seat);
+                        $num_created++;
+                        if ($seat_id) {
+                            update_post_meta($seat_id, 'rrze-rsvp-seat-room', $room_id);
+                            if (is_array($equipment)) {
+                                wp_set_post_terms($seat_id, $equipment, 'rrze-rsvp-equipment');
+                            }
+                        }
+                        $seat++;
+                    }
+                    $row++;
+                    $seat = $seat_start;
+                }
+
+                echo '<div class="notice notice-success is-dismissible">';
+                echo '<h2>' . __('Done!', 'rrze-rsvp') . '</h2>';
+                if ($num_created > 0) {
+                    echo '<p>' . sprintf(_n('%s new seat has been created.', '%s new seats have been created.', $num_created, 'rrze-rsvp'), $num_created) . '</p>';
+                }
+                echo '</div>';
             }
-            echo '</div>';
         }
 
         /*
@@ -126,8 +128,9 @@ class Tools
 
         echo '<form id="rsvp-create-seats" method="get">';
         echo '<input type="hidden" name="page" value="rrze-rsvp-tools">';
+        echo '<input type="hidden" name="status" value="submit">';
         echo '<table class="form-table" role="presentation"><tbody>';
-        echo '<tr><th scope="row"><label for="room_id">' . __('Room','rrze-rsvp') . '</label></th>';
+        echo '<tr><th scope="row"><label for="room_id">' . __('Room', 'rrze-rsvp') . '</label></th>';
         echo '<td>'
             . '<select id="room_id" name="room_id" required>'
             . '<option value="">&mdash; ' . __('Please select', 'rrze-rsvp') . ' &mdash;</option>';
@@ -149,38 +152,38 @@ class Tools
             . '</tr>';
 
         echo '<tr>'
-             . '<th scope="row"><label for="number_seats">' . __('Number of Seats per Row', 'rrze-rsvp') . '</label></th>'
-             . '<td><input type="number" id="number_seats" name="number_seats" min="1" required value="' . $value_num_seats . '"></td>'
-             . '</tr>';
+            . '<th scope="row"><label for="number_seats">' . __('Number of Seats per Row', 'rrze-rsvp') . '</label></th>'
+            . '<td><input type="number" id="number_seats" name="number_seats" min="1" required value="' . $value_num_seats . '"></td>'
+            . '</tr>';
 
         echo '<tr>'
-             . '<th scope="row"><strong>' . __('Rows Numbering System', 'rrze-rsvp') . '</strong></th>'
-             . '<td><input type="radio" id="schema_rows_numeric" name="schema_rows" value="num" required ' . checked($value_schema_rows, 'num', false) . '> <label for="schema_rows_numeric">' . __('Numeric (1 - 999)', 'rrze-rsvp') . '</label><br />'
-             . '<input type="radio" id="schema_rows_a-z" name="schema_rows" value="a-z" ' . checked($value_schema_rows, 'a-z', false) . '> <label  for="schema_rows_a-z">' . __('A-Z', 'rrze-rsvp') . '</label>'
-             . '</td>'
-             . '</tr>';
+            . '<th scope="row"><strong>' . __('Rows Numbering System', 'rrze-rsvp') . '</strong></th>'
+            . '<td><input type="radio" id="schema_rows_numeric" name="schema_rows" value="num" required ' . checked($value_schema_rows, 'num', false) . '> <label for="schema_rows_numeric">' . __('Numeric (1 - 999)', 'rrze-rsvp') . '</label><br />'
+            . '<input type="radio" id="schema_rows_a-z" name="schema_rows" value="a-z" ' . checked($value_schema_rows, 'a-z', false) . '> <label  for="schema_rows_a-z">' . __('A-Z', 'rrze-rsvp') . '</label>'
+            . '</td>'
+            . '</tr>';
 
         echo '<tr class="optional_start_number">'
-             . '<th scope="row"><label for="start_number">' . __('Starting number', 'rrze-rsvp') . ' [' . __('optional', 'rrze-rsvp') . ']</label></th>'
-             . '<td><input type="number" id="start_number" name="start_number" min="1" value="' . $value_start_number . '">'
-             . '<p class="description">' . __('Row numbers will start with 1 by default. You may enter a different starting number, e.g. 101.', 'rrze-rsvp') . '</p>'
-             . '</td>'
-             . '</tr>';
+            . '<th scope="row"><label for="start_number">' . __('Starting number', 'rrze-rsvp') . ' [' . __('optional', 'rrze-rsvp') . ']</label></th>'
+            . '<td><input type="number" id="start_number" name="start_number" min="1" value="' . $value_start_number . '">'
+            . '<p class="description">' . __('Row numbers will start with 1 by default. You may enter a different starting number, e.g. 101.', 'rrze-rsvp') . '</p>'
+            . '</td>'
+            . '</tr>';
 
         echo '<tr>'
-             . '<th scope="row"><strong>' . __('Seats Numbering System', 'rrze-rsvp') . '</strong></th>'
-             . '<td><input type="radio" id="schema_seats_numeric" name="schema_seats" value="num" required ' . checked($value_schema_seats, 'num', false) . '> <label  for="schema_seats_numeric">' . __('Numeric (1 - 999)', 'rrze-rsvp') . '</label><br />'
-             . '<input type="radio" id="schema_seats_a-z" name="schema_seats" value="a-z" ' . checked($value_schema_seats, 'a-z', false) . '> <label  for="schema_seats_a-z">' . __('A-Z', 'rrze-rsvp') . '</label></td>'
-             . '</tr>';
+            . '<th scope="row"><strong>' . __('Seats Numbering System', 'rrze-rsvp') . '</strong></th>'
+            . '<td><input type="radio" id="schema_seats_numeric" name="schema_seats" value="num" required ' . checked($value_schema_seats, 'num', false) . '> <label  for="schema_seats_numeric">' . __('Numeric (1 - 999)', 'rrze-rsvp') . '</label><br />'
+            . '<input type="radio" id="schema_seats_a-z" name="schema_seats" value="a-z" ' . checked($value_schema_seats, 'a-z', false) . '> <label  for="schema_seats_a-z">' . __('A-Z', 'rrze-rsvp') . '</label></td>'
+            . '</tr>';
 
         echo '<tr>'
-             . '<th scope="row"><label for="prefix">' . __('Prefix', 'rrze-rsvp') . ' [' . __('optional', 'rrze-rsvp') . ']</label></th>'
-             . '<td><input type="text" id="prefix" name="prefix" value="' . $value_prefix . '">'
-             . '<p class="description">' . __('Seat names will look like "15A" or "C7" where the first element indicates the row and the second indicates the seat. <br />If the same numbering system ist used for rows and seats, labels will be added automatically: "Row 20 - Seat 3".<br />If a name prefix is given, it will be added before the row, e.g. "1st floor 15A"', 'rrze-rsvp') . '</p>'
-             . '</td>'
-             . '</tr>';
+            . '<th scope="row"><label for="prefix">' . __('Prefix', 'rrze-rsvp') . ' [' . __('optional', 'rrze-rsvp') . ']</label></th>'
+            . '<td><input type="text" id="prefix" name="prefix" value="' . $value_prefix . '">'
+            . '<p class="description">' . __('Seat names will look like "15A" or "C7" where the first element indicates the row and the second indicates the seat. <br />If the same numbering system ist used for rows and seats, labels will be added automatically: "Row 20 - Seat 3".<br />If a name prefix is given, it will be added before the row, e.g. "1st floor 15A"', 'rrze-rsvp') . '</p>'
+            . '</td>'
+            . '</tr>';
 
-        echo '<tr><th scope="row"><strong>' . __('Equipment','rrze-rsvp') . ' [' . __('optional', 'rrze-rsvp') . ']</strong></th>';
+        echo '<tr><th scope="row"><strong>' . __('Equipment', 'rrze-rsvp') . ' [' . __('optional', 'rrze-rsvp') . ']</strong></th>';
         $equipments = get_terms('rrze-rsvp-equipment', ['hide_empty' => false,]);
         $equi_out = '';
         if (!empty($equipments)) {
@@ -189,7 +192,7 @@ class Tools
                     . '<label for="equipment_' . $equipment->term_id . '">' . $equipment->name . '</label></p>';
             }
         } else {
-            $equi_out .= '<p>' . sprintf(__('No equipment found. You can add seat equipment on the %sEquipment screen%s.'), '<a href="'.admin_url('/edit-tags.php?taxonomy=rrze-rsvp-equipment">'), '</a>') . '</p>';
+            $equi_out .= '<p>' . sprintf(__('No equipment found. You can add seat equipment on the %sEquipment screen%s.'), '<a href="' . admin_url('/edit-tags.php?taxonomy=rrze-rsvp-equipment">'), '</a>') . '</p>';
         }
         echo '<td>' . $equi_out;
 
