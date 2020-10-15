1047271b0de0d4a4555f8aa025c368dcf535ff0e
Barbara Bothe <barbara.bothe@fau.de>
2020-10-15 18:09:48 +0200

    Kalender entschwurbelt \o/
    fixes #34

--- rrze-rsvp.php
+++ rrze-rsvp.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE RSVP
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-rsvp
 Description:     FAU Reservation Tool
-Version:         1.7.2-5
+Version:         1.7.2-6
 Author:          RRZE-Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
