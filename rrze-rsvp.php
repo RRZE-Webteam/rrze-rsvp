27b688927612daf5bce6e3df85d50b2f3d6044c8
Cassandre <info@barbara-bothe.de>
2020-12-09 12:03:41 +0100

    Versionsnummer 2.1.0

--- rrze-rsvp.php
+++ rrze-rsvp.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE RSVP
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-rsvp
 Description:     FAU Reservation Tool
-Version:         2.0.7
+Version:         2.1.0
 Author:          RRZE-Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
