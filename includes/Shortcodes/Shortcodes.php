1f4006a94fedf8dcbadb28e69ac6cf341ccb80af
Rolf <rolf.v.d.forst@fau.de>
2022-07-06 15:27:24 +0200

    Update Shortcodes.php

--- includes/Shortcodes/Shortcodes.php
+++ includes/Shortcodes/Shortcodes.php
@@ -4,14 +4,26 @@ namespace RRZE\RSVP\Shortcodes;
 
 defined('ABSPATH') || exit;
 
+use RRZE\RSVP\Auth\{IdM, LDAP};
+
 /**
  * Laden und definieren der Shortcodes
  */
 class Shortcodes
 {
+    protected $idm;
+
+    protected $ldap;
+
+    public function __construct()
+    {
+        $this->idm = new IdM;
+        $this->ldap = new LDAP;
+    }
+
     public function shortcodeAtts($atts, $tag, $settings)
     {
-        // merge given attributes with default ones
+        // Merge given attributes with default ones.
         $defaultAtts = [];
         foreach ($settings as $tagname => $settings) {
             foreach ($settings as $k => $v) {
@@ -21,5 +33,49 @@ class Shortcodes
             }
         }
         return shortcode_atts($defaultAtts[$tag], $atts);
+    }
+
+    protected function authForm($ssoRequired = false, $ldapRequired = false)
+    {
+        $emailError = filter_input(INPUT_GET, 'email_error', FILTER_VALIDATE_INT);
+        $emailError = ($emailError ? '<p class="error-message">' . __('Please login to the account you have used to book this seat.', 'rrze-rsvp') . '</p><br><br>' : '');
+
+        $ldapError = '';
+        if ($ldapRequired && isset($_POST['submit_ldap'])) {
+            $this->ldap->login();
+            if (!$this->ldap->isAuthenticated()) {
+                $ldapError = '<br><p class="error-message">' . __('Login denied', 'rrze-rsvp') . '</p>';
+            }
+        }
+
+        if ($ssoRequired && $this->idm->simplesamlAuth) {
+            $loginUrl = $this->idm->getLoginURL();
+            $idmLogin = sprintf(__('<a href="%s">Please login with your IdM username</a>.', 'rrze-rsvp'), $loginUrl);
+        }
+
+        $title = __('Authentication Required', 'rrze-rsvp');
+
+        $output = '<div class="rrze-rsvp">';
+        $output .= '<h2>' . $title . '</h2>';
+        $output .= $emailError;
+
+        $sOr = '';
+        if ($ssoRequired) {
+            $output .= '<p>' . $idmLogin . '</p>';
+            $sOr = '<br><strong>' . __('Oder', 'rrze-rsvp') . '</strong><br>&nbsp;<br>';
+        }
+
+        if ($ldapRequired) {
+            $output .= $sOr . __('Please login with your UB-AD username', 'rrze-rsvp') . ':' . $ldapError;
+
+            $output .= '<form action="#" method="POST">';
+            $output .= '<label for="username">Username: </label><input id="username" type="text" name="username" value="" />';
+            $output .= '<label for="password">Password: </label><input id="password" type="password" name="password"  value="" />';
+            $output .= '<input type="submit" name="submit_ldap" value="Submit" />';
+            $output .= '</form>';
+        }
+
+        $output .= '</div>';
+        return $output;
     }
 }
