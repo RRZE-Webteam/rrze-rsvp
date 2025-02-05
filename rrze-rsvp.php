ab0a4f58e708d37a2e2f3677d4bd6aaa3e715f53
Rolf <rolf.v.d.forst@fau.de>
2025-02-05 11:07:12 +0100

    Update rrze-rsvp.php

--- rrze-rsvp.php
+++ rrze-rsvp.php
@@ -1,16 +1,18 @@
 <?php
 
 /*
-Plugin Name:     RRZE RSVP
-Plugin URI:      https://github.com/RRZE-Webteam/rrze-rsvp
-Description:     FAU Reservation Tool
-Version:         2.11.4
-Author:          RRZE-Webteam
-Author URI:      https://blogs.fau.de/webworking/
-License:         GNU General Public License v2
-License URI:     http://www.gnu.org/licenses/gpl-2.0.html
-Domain Path:     /languages
-Text Domain:     rrze-rsvp
+Plugin Name:        RRZE RSVP
+Plugin URI:         https://github.com/RRZE-Webteam/rrze-rsvp
+Version:            2.11.5
+Description:        FAU Reservation Tool
+Author:             RRZE-Webteam
+Author URI:         https://blogs.fau.de/webworking/
+License:            GNU General Public License Version 3
+License URI:        https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:        rrze-rsvp
+Domain Path:        /languages
+Requires at least:  6.7
+Requires PHP:       8.2
 */
 
 namespace RRZE\RSVP;
@@ -18,9 +20,6 @@ namespace RRZE\RSVP;
 use RRZE\RSVP\CPT\CPT;
 
 defined('ABSPATH') || exit;
-
-const RRZE_PHP_VERSION = '7.4';
-const RRZE_WP_VERSION = '6.4';
 
 /**
  * Load the configuration file
@@ -38,64 +37,21 @@ register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
 
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
 
-/**
- * Loads a plugin’s translated strings.
- */
-function loadTextdomain()
-{
-    load_plugin_textdomain('rrze-rsvp', false, dirname(plugin_basename(__FILE__)) . '/languages');
-}
-
-/**
- * System requirements verification.
- * @return string Return an error message.
- */
-function systemRequirements(): string
-{
-    $error = '';
-    if (version_compare(PHP_VERSION, RRZE_PHP_VERSION, '<')) {
-        $error = sprintf(
-            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
-            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'rrze-rsvp'),
-            PHP_VERSION,
-            RRZE_PHP_VERSION
-        );
-    } elseif (version_compare($GLOBALS['wp_version'], RRZE_WP_VERSION, '<')) {
-        $error = sprintf(
-            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
-            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'rrze-rsvp'),
-            $GLOBALS['wp_version'],
-            RRZE_WP_VERSION
-        );
-    }
-    return $error;
-}
+// Load the plugin's text domain for localization.
+add_action('init', fn() => load_plugin_textdomain('rrze-rsvp', false, dirname(plugin_basename(__FILE__)) . '/languages'));
 
 /**
  * Activation callback function.
  */
 function activation()
 {
-    loadTextdomain();
-    if ($error = systemRequirements()) {
-        deactivate_plugins(plugin_basename(__FILE__));
-        wp_die(
-            sprintf(
-                /* translators: 1: The plugin name, 2: The error string. */
-                __('Plugins: %1$s: %2$s', 'rrze-rsvp'),
-                plugin_basename(__FILE__),
-                $error
-            )
-        );
-    } else {
-        Users::addRoleCaps();
-        Users::createBookingRole();
+    Users::addRoleCaps();
+    Users::createBookingRole();
 
-        $cpt = new CPT;
-        $cpt->activation();
+    $cpt = new CPT;
+    $cpt->activation();
 
-        flush_rewrite_rules();
-    }
+    flush_rewrite_rules();
 }
 
 /**
@@ -125,33 +81,88 @@ function plugin()
 }
 
 /**
- * Execute on 'plugins_loaded' API/action.
- * @return void
+ * Check system requirements for the plugin.
+ *
+ * This method checks if the server environment meets the minimum WordPress and PHP version requirements
+ * for the plugin to function properly.
+ *
+ * @return string An error message string if requirements are not met, or an empty string if requirements are satisfied.
+ */
+function systemRequirements(): string
+{
+    // Get the global WordPress version.
+    global $wp_version;
+
+    // Get the PHP version.
+    $phpVersion = phpversion();
+
+    // Initialize an error message string.
+    $error = '';
+
+    // Check if the WordPress version is compatible with the plugin's requirement.
+    if (!is_wp_version_compatible(plugin()->getRequiresWP())) {
+        $error = sprintf(
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'rrze-rsvp'),
+            $wp_version,
+            plugin()->getRequiresWP()
+        );
+    } elseif (!is_php_version_compatible(plugin()->getRequiresPHP())) {
+        // Check if the PHP version is compatible with the plugin's requirement.
+        $error = sprintf(
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'rrze-rsvp'),
+            $phpVersion,
+            plugin()->getRequiresPHP()
+        );
+    }
+
+    // Return the error message string, which will be empty if requirements are satisfied.
+    return $error;
+}
+
+/**
+ * Handle the loading of the plugin.
+ *
+ * This function is responsible for initializing the plugin, loading text domains for localization,
+ * checking system requirements, and displaying error notices if necessary.
  */
 function loaded()
 {
-    loadTextdomain();
+    // Trigger the 'loaded' method of the main plugin instance.
     plugin()->loaded();
-    if ($error = systemRequirements()) {
-        add_action('admin_init', function () use ($error) {
+
+    // Check system requirements and store any error messages.
+    if (systemRequirements()) {
+        // If there is an error, add an action to display an admin notice with the error message.
+        add_action('admin_init', function () {
+            $error = systemRequirements();
+            // Check if the current user has the capability to activate plugins.
             if (current_user_can('activate_plugins')) {
-                $pluginData = get_plugin_data(plugin()->getFile());
-                $pluginName = $pluginData['Name'];
+                // Get plugin data to retrieve the plugin's name.
+                $pluginName = plugin()->getName();
+
+                // Determine the admin notice tag based on network-wide activation.
                 $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+
+                // Add an action to display the admin notice.
                 add_action($tag, function () use ($pluginName, $error) {
                     printf(
                         '<div class="notice notice-error"><p>' .
                             /* translators: 1: The plugin name, 2: The error string. */
-                            __('Plugins: %1$s: %2$s', 'rrze-rsvp') .
+                            esc_html__('Plugins: %1$s: %2$s', 'rrze-ac') .
                             '</p></div>',
-                        esc_html($pluginName),
-                        esc_html($error)
+                        $pluginName,
+                        $error
                     );
                 });
             }
         });
+
+        // Return to prevent further initialization if there is an error.
         return;
     }
-    $main = new Main(__FILE__);
-    $main->onLoaded();
+
+    // If there are no errors, create an instance of the 'Main' class and trigger its 'loaded' method.
+    (new Main(__FILE__))->onLoaded();
 }
